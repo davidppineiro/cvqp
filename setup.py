197afc87d7ac20d6cb4a4eff9b3b6a9b189a91a2
davidppineiro <david.p.pineiro@ntnu.no>
2025-09-14 02:24:40 -0700

    Consolidate package metadata to pyproject.toml only

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 import pybind11
 
 ext_modules = [
@@ -11,27 +11,4 @@ ext_modules = [
     ),
 ]
 
-setup(
-    name="cvqp",
-    version="0.1.0",
-    description="A Python implementation of the CVQP solver for CVaR-constrained quadratic programs",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    author="David Pérez Piñeiro, Eric Luxenberg",
-    url="https://github.com/cvxgrp/cvqp",
-    packages=find_packages(),
-    package_data={
-        "cvqp": ["libs/*.cpp", "libs/*.h"],  # Include C++ files
-    },
-    include_package_data=True,
-    ext_modules=ext_modules,
-    install_requires=["pybind11>=2.10.0", "numpy>=2.1.3", "scipy>=1.14.1"],
-    python_requires=">=3.11,<4.0",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.11",
-        "Topic :: Scientific/Engineering :: Mathematics",
-    ],
-)
+setup(ext_modules=ext_modules)
